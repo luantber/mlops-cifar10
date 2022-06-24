b0b79d1b81b4b691c511b26dcafbe638a742dc90
Luis <luantber@gmail.com>
2022-06-24 17:24:06 -0300

    some changes

--- model/predict.py
+++ model/predict.py
@@ -4,22 +4,14 @@ from dataset.dataset import inverted_translation
 import matplotlib.pyplot as plt 
 from torchvision.transforms import functional as F
 
-
 path = "model/epoch=599-step=187800.ckpt"
 model = CNN.load_from_checkpoint(path)
 model.eval()
 
-
 def predict(image, get_dictionary=False):
     
-    image_tensor = torch.tensor(image).view(1, 3, 32, 32)
-    # plt.imshow( F.to_pil_image(image_tensor[0]) )
-    # plt.show()
-
-    print(image_tensor)
+    image_tensor = image.view(1, 3, 32, 32)
     result = model(image_tensor)
-
-    # print( image_tensor.shape )
     result = torch.softmax(result,dim=1)
     result = result[0]
 
@@ -33,7 +25,6 @@ def predict(image, get_dictionary=False):
 
     else:
         best = int(torch.argmax(result))        
-        # print( result, best )
         return inverted_translation[best]
 
 
